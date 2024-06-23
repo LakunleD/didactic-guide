16d78da9ec48ab21130f58509fb5ae21c0c30062
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-06-23 07:07:36 +0100

    Fetch token name

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Address, createPublicClient, http } from 'viem';
+import { sepolia } from 'viem/chains';
+import * as tokenJson from './assets/MyToken.json';
 
 @Injectable()
 export class AppService {
@@ -6,7 +9,23 @@ export class AppService {
 		return 'Hello World!';
 	}
 
-	getContractAddress(): string {
+	getContractAddress(): Address {
 		return "0x2282A77eC5577365333fc71adE0b4154e25Bb2fa";
 	}
+
+	async getTokenName(): Promise<any> {
+		
+		const publicClient = createPublicClient({
+		  chain: sepolia,
+		  transport: http(`https://ethereum-sepolia-rpc.publicnode.com`),
+		});
+
+		const name = await publicClient.readContract({
+		  address: this.getContractAddress(),
+		  abi: tokenJson.abi,
+		  functionName: "name"
+		});
+
+		return name;
+	  }
 }
