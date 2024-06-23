8c053693725fe32ede502eeee75163b972c0987b
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2024-06-23 06:38:34 +0100

    get contract address

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+	getHello(): string {
+		return 'Hello World!';
+	}
+
+	getContractAddress(): string {
+		return "0x2282A77eC5577365333fc71adE0b4154e25Bb2fa";
+	}
 }
